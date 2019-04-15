0fb53f0e43057ec226a6aa22825d41219d59b814
apmurph <alan.murphy@mail.com>
2019-04-15 14:06:41 +0100

    add dynamic data to table with api

--- js/script.js
+++ js/script.js
@@ -60,11 +60,16 @@ $.ajax({
     dataType: 'json',
     type: 'GET',
 }).done(function(response) {
-    // do something with the response, e.g. isolate the id of a linked resource   
-    console.log(response);
-});
 
-//Your API token: 42a42dcc3110469ea789f200ccb94677
+    var standings = response.standings[0].table;
+    console.log(standings);
+    var standing_table_content = document.getElementById("standing_table_content");
+    standings.forEach(function(item) {
+        standing_table_content.innerHTML += '<tr class="standing-table__row" data-item-id="155"><td class="standing-table__cell">' + item.position + '</td> <td class = "standing-table__cell standing-table__cell--name" ><a href="#" class="standing-table__cell--name-link">' + item.team.name + '</a> </td><td class="standing-table__cell">' + item.playedGames + '</td><td class="standing-table__cell">' + item.won + '</td><td class="standing-table__cell">' + item.draw + '</td><td class="standing-table__cell">' + item.lost + '</td><td class="standing-table__cell">' + item.goalsFor + '</td><td class="standing-table__cell">' + item.goalsAgainst + '</td><td class="standing-table__cell">' + item.goalDifference + '</td><td class="standing-table__cell">' + item.points + '</td><td class="standing-table__cell"><div class="standing-table__form"><span class=""> </span><span class=""> </span><span class=""> </span><span class=""> </span><span class=""> </span></div></td></tr > '
+    });
+
+
+});
 
 // ==========================================================================================
 
